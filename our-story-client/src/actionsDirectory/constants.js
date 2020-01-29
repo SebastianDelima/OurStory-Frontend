48ab796f713fd66f984feaeae457539c51e1e44e
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-01-29 18:15:14 -0500

    stories state populated

--- our-story-client/src/actionsDirectory/constants.js
+++ our-story-client/src/actionsDirectory/constants.js
@@ -1,2 +1,4 @@
-export const SET_USERS = 'SET_USERS'
-export const SET_USERS_REQUEST = 'SET_USERS_REQUEST'
+export const SET_USERS           = 'SET_USERS'
+export const SET_USERS_REQUEST   = 'SET_USERS_REQUEST'
+export const SET_STORIES         = 'SET_STORIES'
+export const SET_STORIES_REQUEST = 'SET_STORIES_REQUEST'
